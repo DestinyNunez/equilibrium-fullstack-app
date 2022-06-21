5c7b164acbe123794b4ec02247191078b75a3069
DestinyNunez <88361309+DestinyNunez@users.noreply.github.com>
2022-06-20 22:25:09 -0400

    updated CSS

--- public/main.js
+++ public/main.js
@@ -3,7 +3,7 @@ let trash = document.getElementsByClassName("fa fa-trash");
 
 // affirmations.js
 Array.from(trash).forEach(function(element) {
-  element.addEventListener('click', function(){
+  element.addEventListener('click', function() {
     const _id = this.parentNode.parentNode.id
     console.log(_id)
     fetch('affirmations', {
@@ -14,7 +14,7 @@ Array.from(trash).forEach(function(element) {
       body: JSON.stringify({
         '_id': _id
       })
-    }).then(function (response) {
+    }).then(function(response) {
       window.location.reload()
     })
   });
@@ -22,7 +22,7 @@ Array.from(trash).forEach(function(element) {
 
 // Gratitude.js
 Array.from(trash).forEach(function(element) {
-  element.addEventListener('click', function(){
+  element.addEventListener('click', function() {
     const _id = this.parentNode.parentNode.id
     console.log(_id)
     fetch('gratitude', {
@@ -33,7 +33,7 @@ Array.from(trash).forEach(function(element) {
       body: JSON.stringify({
         '_id': _id
       })
-    }).then(function (response) {
+    }).then(function(response) {
       window.location.reload()
     })
   });
@@ -41,7 +41,7 @@ Array.from(trash).forEach(function(element) {
 
 // meditation.js
 Array.from(trash).forEach(function(element) {
-  element.addEventListener('click', function(){
+  element.addEventListener('click', function() {
     const _id = this.parentNode.parentNode.id
     console.log(_id)
     fetch('meditation', {
@@ -52,7 +52,7 @@ Array.from(trash).forEach(function(element) {
       body: JSON.stringify({
         '_id': _id
       })
-    }).then(function (response) {
+    }).then(function(response) {
       window.location.reload()
     })
   });
@@ -60,7 +60,7 @@ Array.from(trash).forEach(function(element) {
 
 // journal.js
 Array.from(trash).forEach(function(element) {
-  element.addEventListener('click', function(){
+  element.addEventListener('click', function() {
     const _id = this.parentNode.parentNode.id
     console.log(_id)
     fetch('journal', {
@@ -71,10 +71,51 @@ Array.from(trash).forEach(function(element) {
       body: JSON.stringify({
         '_id': _id
       })
-    }).then(function (response) {
+    }).then(function(response) {
       window.location.reload()
     })
   });
+});
+
+
+
+// meditation Data
+const ctx = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+    datasets: [{
+      label: 'Meditation Data',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132)',
+        'rgba(54, 162, 235)',
+        'rgba(255, 206, 86)',
+        'rgba(75, 192, 192)',
+        'rgba(153, 102, 255)',
+        'rgba(255, 159, 64)',
+        'rgba(198, 178, 298)'
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+        'rgba(198, 178, 298)'
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
 });
 
 
